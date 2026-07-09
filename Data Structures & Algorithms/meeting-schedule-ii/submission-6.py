f7e6e2c64513e1596df02f27ea85457a4a794882
Zan Qureshi <zanq.mw@gmail.com>
2026-07-09 00:07:42 -0400

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        times = defaultdict(list)
+        for i, interval in enumerate(intervals):
+            print(interval)
+            start, end = interval.start, interval.end
+            times[start].append(i)
+            times[end].append(i)
+        res = 0
+        sort = sorted(times.keys())
+        s = set()
+        for t in sort:
+            for meet in times[t]:
+                if meet in s:
+                    s.remove(meet)
+                else:
+                    s.add(meet)
+            res = max(res, len(s))
+
+        return res
+
